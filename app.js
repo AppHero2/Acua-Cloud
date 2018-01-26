72c78945c905ea64c7c6999a2721f4934dae9a24
AppHero2 <bearkingbrother@gmail.com>
2018-01-27 02:31:05 +0800

    fixed notification issue

--- app.js
+++ app.js
@@ -63,15 +63,16 @@ function Acua_Cloud() {
         http.get('https://acua-node.herokuapp.com/');
     },300000);
 
-    setInterval(function(){
-        doManageNotification();
-    }, 60000);
+    // setInterval(function(){
+    //     doManageNotification();
+    // }, 60000);
+    doManageNotification();
   }
 
   function doManageNotification(){
     var date = new Date();
     var currentTime = date.getTime();
-    var query = firebase.database().ref('Orders').orderByChild('serviceStatus').equalTo('PENDING');
+    var query = firebase.database().ref('Orders').orderByChild('is24reminded').equalTo(false);
     query.once('value', function(snapshot){
       if(snapshot.val() != null) {
         var pushTokens = new Array();
@@ -79,9 +80,26 @@ function Acua_Cloud() {
           var customerId = obj.val().customerId;
           var customerPushToken = obj.val().customerPushToken;
           var beginAt = obj.val().beginAt;
-          var prevAt = beginAt - 86400000;
-          if (prevAt <= currentTime && currentTime < prevAt+60000 && customerPushToken!=null) {
+          var prevAt = beginAt - 86400000; // 24hr
+          var nextAt = beginAt;//beginAt - 86000000; // 23.8
+          if (prevAt <= currentTime && currentTime < nextAt && customerPushToken!=null) {
             pushTokens.push(customerPushToken);
+
+            // firebase.database().ref('Orders').child(obj.key).child('is24reminded').set(true);
+            var notificationRef = firebase.database().ref('Notifications').child(customerId).push();
+            var notificationKey = notificationRef.key; 
+            var notification = {
+              'idx': notificationKey,
+              'createdAt': currentTime,
+              'isRead' : false,
+              'title' : 'Reminder',
+              'message' : 'The 24-hour countdown to your acua experience has begun' 
+            };
+            notificationRef.set(notification, function(error){
+              if (error) {
+                  console.log('notification : ', error);
+              }
+            });
           }
 
           if (pushTokens.length > 0) {
@@ -89,10 +107,12 @@ function Acua_Cloud() {
             var message = { 
               app_id: "1f9e701b-7709-40e6-a1b6-7dff0ee29b42",
               contents: {"en": "The 24-hour countdown to your acua experience has begun"},
-              included_segments: pushTokens
+              // included_segments: ["Active Users"],
+              include_player_ids: pushTokens,
             };
             
             sendNotification(message);
+
           }
         });
       }
@@ -135,13 +155,13 @@ function Acua_Cloud() {
     req.end();
   };
 
-}
+};
 
 Acua_Cloud.startInstance = () => {
   var acua_cloud = new Acua_Cloud();
   acua_cloud.start();
   return acua_cloud;
-}
+};
 
 Acua_Cloud.startInstance();
 
