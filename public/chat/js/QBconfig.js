e326b8003691ff476b8dd14927d7bed36c982537
AppHero2 <bearkingbrother@gmail.com>
2018-04-27 10:10:20 +0800

    chat

---
+++ public/chat/js/QBconfig.js
@@ -0,0 +1,44 @@
+var QBconfig = {
+    credentials: {
+        appId: 70258,
+        authKey: 'Ggs7HAXSnGB-Aed',
+        authSecret: 'yvSFsaqzce2qZxv'
+    },
+    appConfig: {
+        chatProtocol: {
+            active: 2
+        },
+        streamManagement: {
+            enable: true
+        },
+        debug: {
+            mode: 1,
+            file: null
+        }
+    }
+};
+
+var appConfig = {
+    dilogsPerRequers: 15,
+    messagesPerRequest: 50,
+    usersPerRequest: 15,
+    typingTimeout: 3 // 3 seconds
+};
+
+var CONSTANTS = {
+    DIALOG_TYPES: {
+        CHAT: 3,
+        GROUPCHAT: 2,
+        PUBLICCHAT: 1
+    },
+    ATTACHMENT: {
+        TYPE: 'image',
+        BODY: '[attachment]',
+        MAXSIZE: 2 * 1000000, // set 2 megabytes,
+        MAXSIZEMESSAGE: 'Image is too large. Max size is 2 mb.'
+    },
+    NOTIFICATION_TYPES: {
+        NEW_DIALOG: '1',
+        UPDATE_DIALOG: '2'
+    }
+};
