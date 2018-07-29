6dd1949e5054055960c21f80c2374d93f857188e
AppHero2 <bearkingbrother@gmail.com>
2018-07-30 01:19:41 +0800

    email router

---
+++ routes/email.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+      user: process.env.GMAIL_ACCOUNT,
+      pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+router.post('/send', (req, res, next) => {
+
+    const subject = req.body.subject;
+    const text = req.body.text;
+    const html = req.body.html;
+
+    // setup email data with unicode symbols
+    const mailOptions = {
+        from: process.env.GMAIL_ACCOUNT, // sender address
+        to: process.env.CONTACT_EMAIL, // list of receivers
+        subject: subject, // Subject line
+        text: text, // plain text body
+        html: html // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    }); 
+
+});
+
+module.exports = router;
+
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var payment = require('./payment');
+var email = require('./email');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.use('/payment', payment);
+router.use('./email', email);
 
 module.exports = router;
